b6661e45e48df481f7ba7a0af187ddda719d1471
ThanhVui <thanhvuikg100@gmail.com>
2025-02-20 16:28:45 +0700

    Upload Category

--- src/main/java/dals/CategoryDAO.java
+++ src/main/java/dals/CategoryDAO.java
@@ -4,10 +4,29 @@
  */
 package dals;
 
+import java.util.List;
+import models.Categories;
+import utils.DBContext;
+
 /**
  *
  * @author Nguyen Huy Hoang - CE182102
  */
-public class CategoryDAO {
+public class CategoryDAO extends DBContext{
+
+    public List<Categories> getAllCategories() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public void createCategory(Categories category) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public void deleteRoomType(int categoryID) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
     
+    public static void main(String[] args) {
+        
+    }
 }
