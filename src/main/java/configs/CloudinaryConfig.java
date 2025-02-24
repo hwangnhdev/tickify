e7077ede4e192d990e971da13485d497ea0178fa
ThanhVui <thanhvuikg100@gmail.com>
2025-02-24 16:25:44 +0700

    Update

--- src/main/java/configs/CloudinaryConfig.java
+++ src/main/java/configs/CloudinaryConfig.java
@@ -13,20 +13,5 @@ import com.cloudinary.utils.ObjectUtils;
  */
 public class CloudinaryConfig {
 
-    private static final String DB_URL = "";
-    private static final String DB_USER = "";
-    private static final String DB_PASSWORD = "";
 
-    private static final String CLOUD_NAME = "dnvpphtov";
-    private static final String API_KEY = "361735526766643";
-    private static final String API_SECRET = "tqw6jzCoHlzq4FN9Vusrbllobio";
-
-    public static Cloudinary getInstance() {
-        return new Cloudinary(ObjectUtils.asMap(
-                "cloud_name", CLOUD_NAME,
-                "api_key", API_KEY,
-                "api_secret", API_SECRET,
-                "secure", true
-        ));
-    }
 }
