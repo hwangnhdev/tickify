e3677ecb2e4ac0d6b000ba7b92e45bd935c0005c
ThanhVui <thanhvuikg100@gmail.com>
2025-02-24 20:18:06 +0700

    Merge

--- src/main/java/configs/VnPayConfig.java
+++ src/main/java/configs/VnPayConfig.java
@@ -10,9 +10,4 @@ package configs;
  */
 public class VnPayConfig {
 
-    public static String vnp_PayUrl = "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html";
-    public static String vnp_ReturnUrl = "http://localhost:8080/vnpay_jsp/vnpay_return.jsp";
-    public static String vnp_TmnCode = "4RQE08EO";
-    public static String secretKey = "GOM94YUV3NU1QWT5NXTUZPNVF1ERU5FH";
-    public static String vnp_ApiUrl = "https://sandbox.vnpayment.vn/merchant_webapi/api/transaction";
 }
